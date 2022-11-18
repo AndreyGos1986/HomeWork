41e9d7f58c9dfb315ec9f8174b3f26dabe57d645
Андрей <gosudarev_a@mail.ru>
2022-11-18 07:59:47 +0300

    Initial commit

---
+++ src/test/java/gos/andrey1986/homework/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package gos.andrey1986.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
